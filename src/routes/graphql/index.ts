8eae2b0775c12eb48f322c77ff9d087a6a5cac43
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2023-07-24 14:51:03 +0400

    Implemented schema which passes gql-rule test

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema, Schema } from './schemas.js';
-import { GraphQLArgs, graphql } from 'graphql';
+import { GraphQLArgs, graphql, validate, parse } from 'graphql';
+import depthLimit from 'graphql-depth-limit'
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
 
@@ -19,6 +20,10 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       // console.log(req.url);
       // console.log(req.method);
       // console.log(req.body.query);
+      const gqlErrs = validate( Schema, parse( req.body.query ), [depthLimit(5)], { maxErrors: 1 } );
+      if ( gqlErrs.length > 0 ) {
+         return { errors: gqlErrs };
+      }
       const gqlArgs:GraphQLArgs = {schema: Schema, source: req.body.query, contextValue: { prisma: prisma } };
       if ( req.body.variables) {
         // console.log(req.body.variables);
@@ -26,11 +31,14 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       }
       try {
         const result = await graphql(gqlArgs);
+        // if ( result.errors?.length ) {
+        //   console.log(result.errors);
+        // }
         return result;
       } catch (error) {
-          console.log(error);        
-          return {};
-        }
+        // console.log(error);
+        return { errors: [error] };
+      }
     },
   });
 };
